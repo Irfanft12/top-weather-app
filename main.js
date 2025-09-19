2a91a021c3c5eccb91f997ae45485f9ba0d4feb4
Irfanft12 <irfanft12@gmail.com>
2025-09-19 17:07:29 +0530

    loading feature added

--- main.js
+++ main.js
@@ -3,19 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const input = document.querySelector("input");
     const button = document.querySelector("button");
     const weatherDisplay = document.querySelector(".weather-display");
-    const tempText = weatherDisplay.querySelector(".temp-text");
-    const location = weatherDisplay.querySelector(".location");
-    const condition = weatherDisplay.querySelector(".condition");
+    
     const searchError = document.querySelector(".search-error");
-    const feelsLike = weatherDisplay.querySelector(".feels-like");
-    const description = weatherDisplay.querySelector(".description");
-    const iconImage = weatherDisplay.querySelector(".icon-image");
     
     
     
 
     async function getWeather(location) {
         searchError.innerHTML = ``;
+        weatherDisplay.innerHTML = "Loading...";
         try {
             const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=uk&key=WHB4ULD6X9GXRXXTWZ3SR3C9M&contentType=json`);
             if (!response.ok) {
@@ -39,14 +35,24 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function processData(data) {
-        console.log(data.days[0].icon)
-        const text = data.currentConditions.temp
-        location.innerHTML = capitalizeFistLetter(data.address);
-        tempText.innerHTML = text + "°C";
-        condition.innerHTML = data.currentConditions.conditions;
-        feelsLike.innerHTML =  `Feels like ${data.currentConditions.feelslike}°C`;
-        description.innerHTML = data.description;
-        iconImage.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/4th%20Set%20-%20Color/${data.days[0].icon}.png`;
+        const html = `
+            <div class="row">
+                <div class="column">
+                    <h2 class="location">${capitalizeFistLetter(data.address)}</h2>
+                    <h4 class="temp">
+                        <img src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/4th%20Set%20-%20Color/${data.days[0].icon}.png" alt="${data.days[0].icon}" class="icon-image">
+                        <span class="temp-text">${data.currentConditions.temp} °C </span>
+                        <p class="feels-like">Feels like ${data.currentConditions.feelslike}°C</p>
+                    </h4>
+                    <p class="condition">${data.currentConditions.conditions}</p>
+                </div>
+                <div class="column">
+                    <p class="description">${data.description}</p>
+                </div>
+            </div>
+        `;
+
+        weatherDisplay.innerHTML = html;
     }
 
     form.addEventListener("submit", function(e) {
